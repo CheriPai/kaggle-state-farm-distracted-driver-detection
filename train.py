28c0b7757a2c481a45547e1c3fc69735ccb5ebd7
dat <datdo@rocketmail.com>
2017-01-08 10:57:06 -0800

    Tune parameters and save weights

--- train.py
+++ train.py
@@ -3,13 +3,39 @@ from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
 img_size = 224
-epochs = 5
+batch_size = 64
+dropout = 0.1
+
+lr = 0.001
+decay = 1e-6
+epochs = 10
 
 gen = ImageDataGenerator()
-train = gen.flow_from_directory("data/valid", target_size=(img_size, img_size), batch_size=32, class_mode="categorical", shuffle=True)
-valid = gen.flow_from_directory("data/sample", target_size=(img_size, img_size), batch_size=32, class_mode="categorical", shuffle=False)
+train = gen.flow_from_directory(
+    "data/train",
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode="categorical",
+    shuffle=True)
+valid = gen.flow_from_directory(
+    "data/valid",
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode="categorical",
+    shuffle=False)
 
-vgg = VGG16(0.1)
+vgg = VGG16(dropout)
 vgg.finetune_dense(10)
-vgg.model.compile(optimizer=Adam(), loss="categorical_crossentropy", metrics=["accuracy"])
-vgg.model.fit_generator(train, samples_per_epoch=train.nb_sample, nb_epoch=epochs, validation_data=valid, nb_val_samples=valid.nb_sample)
+
+vgg.model.compile(
+    optimizer=Adam(lr=lr, decay=decay),
+    loss="categorical_crossentropy",
+    metrics=["accuracy"])
+vgg.model.fit_generator(
+    train,
+    samples_per_epoch=train.nb_sample,
+    nb_epoch=epochs,
+    validation_data=valid,
+    nb_val_samples=valid.nb_sample)
+
+vgg.model.save_weights("data/weights.h5")
