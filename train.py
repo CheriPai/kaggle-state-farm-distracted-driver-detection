a4bf9f878415f0682e43cc9a2fe0af09bd2f1da3
dat <datdo@rocketmail.com>
2017-01-15 13:36:08 -0800

    Use pseudolabeling

--- train.py
+++ train.py
@@ -1,14 +1,15 @@
+import numpy as np
 from vgg16 import VGG16
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
 img_size = 224
 batch_size = 64
-dropout = 0.1
+dropout = 0.5
 
-lr = 0.001
-decay = 1e-6
-epochs = 25
+lr = 0.0001
+decay = 0
+epochs = 5
 
 TRAIN_PATH = "data/train"
 VALID_PATH = "data/valid"
@@ -36,6 +37,12 @@ valid = gen.flow_from_directory(
     batch_size=batch_size,
     class_mode="categorical",
     shuffle=False)
+pseudo = gen.flow_from_directory(
+    VALID_PATH,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode="categorical",
+    shuffle=True)
 
 vgg = VGG16(dropout)
 vgg.finetune_dense(10)
@@ -44,11 +51,24 @@ vgg.model.compile(
     optimizer=Adam(lr=lr, decay=decay),
     loss="categorical_crossentropy",
     metrics=["accuracy"])
-vgg.model.fit_generator(
-    train,
-    samples_per_epoch=train.nb_sample,
-    nb_epoch=epochs,
-    validation_data=valid,
-    nb_val_samples=valid.nb_sample)
+
+vgg.model.load_weights("data/weights.h5")
+
+for i in range(epochs):
+    vgg.model.fit_generator(
+        train,
+        samples_per_epoch=train.nb_sample,
+        nb_epoch=1,
+        validation_data=valid,
+        nb_val_samples=valid.nb_sample)
+
+    pseudo.__dict__["classes"] = np.argmax(vgg.model.predict_generator(valid, val_samples=valid.nb_sample), axis=-1)
+
+    vgg.model.fit_generator(
+        pseudo,
+        samples_per_epoch=pseudo.nb_sample,
+        nb_epoch=1,
+        validation_data=valid,
+        nb_val_samples=valid.nb_sample)
 
 vgg.model.save_weights("data/weights.h5")
